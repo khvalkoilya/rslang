df4f3936cc9c64cfa12f926ae71faad68c24ef22
Andey Amelchenia <pmad1986@gmail.com>
2020-06-16 16:52:14 +0300

    feat: setup config, add entry points

---
+++ src/App/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>My React App!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// eslint-disable-next-line import/extensions
+import App from './App/App.jsx';
+import './style.scss';
+
+ReactDOM.render(<App />, document.getElementById('root'));
