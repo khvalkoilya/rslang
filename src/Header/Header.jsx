e36eb7600d8bd77835095bae76b3d8238bc16140
Aleksand Sumaneev <cahobad@ya.ru>
2020-06-18 00:57:45 +0300

    feat: add base structure for header

---
+++ src/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Header.scss';
+
+const Header() {
+    return(
+        <div className="header">
+            header
+        </div>
+    );
+} 
+
+export default Header;
