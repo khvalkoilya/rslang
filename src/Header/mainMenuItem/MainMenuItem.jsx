b7075e021613b627c37df669d4e4da83003b7985
khvalkoilya <49587558+khvalkoilya@users.noreply.github.com>
2020-06-19 22:57:47 +0300

    feat: Implement Main menu (#29)
    * init: start rslang-13
    
    * feat: add base structure for header
    
    * feat: modified App.jsx
    
    * feat: header done
    
    * fix: header
    
    * feat: add main menu item
    
    * feat: import component to app
    
    * feat: add constant array for main menu
    
    * feat: cherry pick constants from 25 branch
    
    * fix: fix export  name
    
    * feat: add testing of onclick callback
    
    * feat: realize showing main menu for guest and for user
    
    * feat: realize main menu function for header
    
    * docs: add .vscode to .gitignore
    
    * fix: remove comments
    
    * fix: delete folder main menu from src, because we have such folder in Header
    
    * feat: rewrite className for main menu item and onclick for button, also add variable startPage
    
    * fix: rename folders in underscore
    
    * feat: create folder and file mainMenu and replace mainMenu class there
    
    * fix: fix constant name
    
    * feat: return className for main menu
    
    Co-authored-by: Aleksand Sumaneev <cahobad@ya.ru>
    Co-authored-by: Ivan <kan.ivann@gmail.com>
    Co-authored-by: Artem <uspenskiyart@gmail.com>

---
+++ src/Header/mainMenuItem/MainMenuItem.jsx
@@ -0,0 +1,34 @@
+import React, { useCallback } from 'react';
+import PropTypes from 'prop-types';
+import './MainMenuItem.scss';
+
+const MainMenuItem = ({
+  name, isUnderlined, id, isAuthorized, setPage, isAuth,
+}) => (
+  <button
+    type="button"
+    onClick={isAuth ? useCallback(() => {
+      setPage(id);
+    }, []) : null}
+    className={`menu-item ${isUnderlined ? 'menu-item-active' : ''} ${!isAuth && !isAuthorized ? 'menu-item-lock' : ''}`}
+  >
+    { name }
+  </button>
+);
+
+MainMenuItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  isUnderlined: PropTypes.bool,
+  id: PropTypes.number,
+  isAuthorized: PropTypes.bool,
+  setPage: PropTypes.func.isRequired,
+  isAuth: PropTypes.bool.isRequired,
+};
+
+MainMenuItem.defaultProps = {
+  isUnderlined: false,
+  id: 0,
+  isAuthorized: false,
+};
+
+export default MainMenuItem;
