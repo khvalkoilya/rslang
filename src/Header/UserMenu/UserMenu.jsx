6b073db04011ef9798da9f002328de2ef504b4f6
Андрей Амельченя <60568794+AndreyAmelchenia@users.noreply.github.com>
2020-06-19 22:40:41 +0300

    [rslang-16]: create user menu (#24)
    * feat: add UserMenu
    
    * fix: App.jsx
    
    * fix: UserMenu
    
    * fix: UserMenu.jsx
    
    * fix: UserMenu.jsx
    
    * fix: _userMenu.scss add class block, none, user_menu
    
    * fix: name Menu

---
+++ src/Header/UserMenu/UserMenu.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './_userMenu.scss';
+
+const UserMenu = ({ isVisible }) => {
+  const [visible, setVisible] = useState(false);
+  const title = isVisible ? 'Меню' : 'Гость';
+  return (
+    <>
+      <button type="button" onClick={() => setVisible(!visible)}>{title}</button>
+      <div className={`user__menu ${visible ? 'block' : 'none'}`}>привет</div>
+    </>
+  );
+};
+
+UserMenu.propTypes = {
+  isVisible: PropTypes.bool.isRequired,
+};
+
+export default UserMenu;
