bf30c72f110ac8c291585bbfab926dc994e8c1bb
Dzmitrysiniakou <jack.pott@mail.ru>
2020-06-20 12:36:31 +0300

    fix name footer's function

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -2,25 +2,23 @@ import React from 'react';
 
 import './_footer.scss';
 
-function Footer() {
-  return (
-    <div className="footerWrapper">
-      <div className="footerContainer">
-        <a href="https://google.com">About us</a>
-        <div>
-          <a href="https://google.com" className="footerFedbackLink">
-            <img src="./assets/images/icoMail.png" alt="icoMail" />
-          </a>
-          <a href="https://youtube.com" className="footerFedbackLink">
-            <img src="./assets/images/icoYoutube.png" alt="icoYoutube" />
-          </a>
-          <a href="https://telegram.com" className="footerFedbackLink">
-            <img src="./assets/images/icoTelegram.png" alt="icoTelegram" />
-          </a>
-        </div>
+const Footer = () => (
+  <div className="footerWrapper">
+    <div className="footerContainer">
+      <a href="https://google.com">About us</a>
+      <div>
+        <a href="https://google.com" className="footerFedbackLink">
+          <img src="./assets/images/icoMail.png" alt="icoMail" />
+        </a>
+        <a href="https://youtube.com" className="footerFedbackLink">
+          <img src="./assets/images/icoYoutube.png" alt="icoYoutube" />
+        </a>
+        <a href="https://telegram.com" className="footerFedbackLink">
+          <img src="./assets/images/icoTelegram.png" alt="icoTelegram" />
+        </a>
       </div>
     </div>
-  );
-}
+  </div>
+);
 
 export default Footer;
