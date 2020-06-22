efe3573c340395f2c5603e7bae8512c6c3b97407
Andey Amelchenia <pmad1986@gmail.com>
2020-06-22 22:25:24 +0300

    fix: function getWordsData

--- src/utils/utilsApi.js
+++ src/utils/utilsApi.js
@@ -5,23 +5,9 @@ export const api = (page, group) => fetch(URL.words.getWords(page, group))
   .then((result) => result)
   .catch((error) => error);
 
-export const getId = async (fn, group = 0, quantity = 20) => {
-  const arr = [];
-  for (let i = 0; i <= 29; i += 1) {
-    arr.push(api(i, group));
-  }
-  const arrWords = await Promise.all(arr);
-  const idWords = [];
-  arrWords.forEach((el) => {
-    el.forEach((elem) => {
-      idWords.push(elem);
-    });
-  });
-  const randomId = idWords
-    .map((a) => ({ sort: Math.random(), value: a }))
-    .sort((a, b) => a.sort - b.sort)
-    .map((a) => a.value);
-  return fn(randomId.slice(0, quantity));
+export const getId = async (fn, group = 0, page = 0) => {
+  const arrWords = await api(group, page);
+  return fn(arrWords);
 };
 
 export const getUrlData = (name) => `https://raw.githubusercontent.com/AndreyAmelchenia/rslang-data/master/${name}`;
