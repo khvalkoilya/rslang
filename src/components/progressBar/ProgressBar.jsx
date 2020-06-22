9bb68ed6c3b3ae518c6874adc5dca7552292f936
Artem <uspenskiyart@gmail.com>
2020-06-22 21:59:33 +0300

    fix: fix progress bar data

--- src/components/progressBar/ProgressBar.jsx
+++ src/components/progressBar/ProgressBar.jsx
@@ -3,27 +3,24 @@ import PropTypes from 'prop-types';
 import ProgressBar from '@ramonak/react-progress-bar';
 import './_ProgressBar.scss';
 
-const UserProgressBar = ({doneCards ,maxCards}) => {
-  //const [number, changeNumber] = React.useState(currentCard);
-  return  (
-    <div className='userProgress'>
-      <span className='numberCards'>15</span>
-      <div className='userProgress-bar'>
-        <ProgressBar bgcolor='#d1af6c' completed={60} height="24px" labelSize='0' borderRadius='0' baseBgColor='#fff'/>
-      </div>
-      <span className='numberCards'>25</span>
+const UserProgressBar = ({ doneCards, maxCards }) => (
+  <div className="userProgress">
+    <span className="numberCards">{doneCards}</span>
+    <div className="userProgress-bar">
+      <ProgressBar completed={(doneCards / maxCards) * 100} bgcolor="#d1af6c" width="80%" height="24px" labelSize="0" borderRadius="0" baseBgColor="#fff" />
     </div>
-  )
-}
+    <span className="numberCards">{maxCards}</span>
+  </div>
+);
 
 UserProgressBar.propTypes = {
   doneCards: PropTypes.number,
   maxCards: PropTypes.number,
-}
+};
 
 UserProgressBar.defaultProps = {
   doneCards: 0,
   maxCards: 20,
-}
+};
 
 export default UserProgressBar;
