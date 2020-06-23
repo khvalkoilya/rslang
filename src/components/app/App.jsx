5f95451ac6b3ae1e9113dd2d590a74f1cb01ee80
Aleksand Sumaneev <cahobad@ya.ru>
2020-06-24 01:18:39 +0300

    feat: add main and style

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
+import Main from '../main/Main';
 
 const App = () => (
   <>
     <Header />
+    <Main />
     <Footer />
   </>
 );
