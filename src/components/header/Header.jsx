7c56302a7371828124b579371fbecee8ad557477
KanIvan <60774013+KanIvan@users.noreply.github.com>
2020-06-23 14:48:50 +0300

    feat: added logo to page (#44)

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import MainMenu from '../mainMenu/MainMenu';
+import Logo from '../logo/Logo';
 
 const Header = () => (
   <div className="header">
-    <div className="header__logo" />
+    <div className="header__logo"><Logo /></div>
     <div className="header__main-menu">
       <MainMenu />
     </div>
