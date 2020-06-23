67effeb31c8f809e15975fe36fe2657475d96648
Aleksand Sumaneev <cahobad@ya.ru>
2020-06-24 00:52:15 +0300

    fix: eslint busg

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,18 +4,15 @@ import UserMenu from '../userMenu/UserMenu';
 
 const isAuth = true;
 
-function Header() {
-  return (
-    <div className="header">
-      <div className="header__logo" />
-      <div className="header__main-menu">
-        <MainMenu isAuth={isAuth} />
-      </div>
-      <div className="header__profile-menu">
-        <UserMenu isAuth={isAuth} />
-      </div>
+const Header = () => (
+  <div className="header">
+    <div className="header__logo" />
+    <div className="header__main-menu">
+      <MainMenu isAuth={isAuth} />
     </div>
-    <div className="header__profile-menu" />
+    <div className="header__profile-menu">
+      <UserMenu isAuth={isAuth} />
+    </div>
   </div>
 );
 
