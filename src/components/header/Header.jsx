dfb9dd51ad000df82d17ca3b46efd41f531350a4
Artem <uspenskiyart@gmail.com>
2020-06-25 00:10:31 +0300

    feat: add svg && add style constant && add small screen styles for header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainMenu from '../mainMenu/MainMenu';
 import UserMenu from '../userMenu/UserMenu';
+import BurgerButton from '../burgerButton/BurgerButton';
 import Logo from '../logo/Logo';
 
 const isAuth = true;
 
-const Header = () => (
-  <div className="header">
-    <div className="header__logo">
-      <Logo />
+const Header = () => {
+  const [visible, setVisible] = useState(false);
+  const Click = () => setVisible(!visible);
+  return (
+    <div className="header">
+      <BurgerButton click={Click} />
+      <div className="header__logo">
+        <Logo />
+      </div>
+      <nav className={`nav ${visible ? 'visible' : 'unvisible'}`}>
+        <div className="header__main-menu">
+          <MainMenu isAuth={isAuth} />
+        </div>
+        <div className="header__profile-menu">
+          <UserMenu isAuth={isAuth} />
+        </div>
+      </nav>
     </div>
-    <nav>
-      <div className="header__main-menu">
-        <MainMenu isAuth={isAuth} />
-      </div>
-      <div className="header__profile-menu">
-        <UserMenu isAuth={isAuth} />
-      </div>
-    </nav>
-  </div>
-);
+  );
+};
 
 export default Header;
