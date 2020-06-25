af8e08b47c41bdb302f8665c7ed6d1a00ae72f01
khvalkoilya <khvalko.ilya@gmail.com>
2020-06-25 16:51:06 +0300

    feat: add style for transate

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -7,12 +7,12 @@ import { getLetterArr } from './utils';
 const Card = ({
   word,
   image,
+  wordTranslate,
   textMeaning,
   textExample,
   transcription,
   textExampleTranslate,
   textMeaningTranslate,
-  wordTranslate,
 }) => {
   const [innerWord, setInnerWord] = useState('');
   const [defaultVal, setDefaultVal] = useState([]);
@@ -20,25 +20,27 @@ const Card = ({
   const compareWords = () => setDefaultVal(getLetterArr(word, innerWord));
 
   return (
-    <div className="wrapper">
-      <Input
-        word={word}
-        wordLen={word.length}
-        setWord={setInnerWord}
-        defaultVal={defaultVal}
-      />
-      <div>{textMeaning}</div>
-      <div>{textExample}</div>
-      <div>{transcription}</div>
-      <div>{textExampleTranslate}</div>
-      <div>{textMeaningTranslate}</div>
-      <div>{wordTranslate}</div>
-      <img src={image} alt={word} />
-      <button type="button">выкл перевод</button>
-      <button type="button">вкл воспр</button>
-      <button type="button">удалить</button>
-      <button type="button">сложно</button>
-      <button type="button" onClick={compareWords}>далее</button>
+    <div className="card">
+      <div className="card-wrapper">
+        <Input
+          word={word}
+          wordLen={word.length}
+          setWord={setInnerWord}
+          defaultVal={defaultVal}
+        />
+        <div className="card__translate">{wordTranslate}</div>
+        <div>{textMeaning}</div>
+        <div>{textExample}</div>
+        <div>{transcription}</div>
+        <div>{textExampleTranslate}</div>
+        <div>{textMeaningTranslate}</div>
+        <img src={image} alt={word} />
+        <button type="button">выкл перевод</button>
+        <button type="button">вкл воспр</button>
+        <button type="button">удалить</button>
+        <button type="button">сложно</button>
+        <button type="button" onClick={compareWords}>далее</button>
+      </div>
     </div>
   );
 };
@@ -46,23 +48,23 @@ const Card = ({
 Card.propTypes = {
   word: PropTypes.string,
   image: PropTypes.string,
+  wordTranslate: PropTypes.string,
   textMeaning: PropTypes.string,
   textExample: PropTypes.string,
   transcription: PropTypes.string,
   textExampleTranslate: PropTypes.string,
   textMeaningTranslate: PropTypes.string,
-  wordTranslate: PropTypes.string,
 };
 
 Card.defaultProps = {
-  word: 'word',
+  word: 'Run',
   image: 'image',
+  wordTranslate: 'Бежать, бегать',
   textMeaning: 'textMeaning',
   textExample: 'textExample',
   transcription: 'transcription',
   textExampleTranslate: 'textExampleTranslate',
   textMeaningTranslate: 'textMeaningTranslate',
-  wordTranslate: 'wordTranslate',
 };
 
 export default Card;
