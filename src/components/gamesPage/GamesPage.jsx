84a8c34c6271b54e41d6cff65639921d05bfecec
Aleksand Sumaneev <cahobad@ya.ru>
2020-06-28 20:58:08 +0300

    fix: add images for games

--- src/components/gamesPage/GamesPage.jsx
+++ src/components/gamesPage/GamesPage.jsx
@@ -4,27 +4,39 @@ const GamesPage = () => (
   <div className="games-page">
     <div className="games-page__block">
       <h2 className="games-page__heading">SpeackIt</h2>
-      <img className="game-page__image" src="../../assets/images/logo.svg" alt="game SpeackIt" />
+      <div className="game-page__image-block">
+        <img className="game-page__image" src="../../assets/images/games-page-icons/interaction.svg" alt="game SpeackIt" />
+      </div>
     </div>
     <div className="games-page__block">
       <h2 className="games-page__heading">English puzzle</h2>
-      <img className="game-page__image" src="../../assets/images/logo.svg" alt="game SpeackIt" />
+      <div className="game-page__image-block">
+        <img className="game-page__image" src="../../assets/images/games-page-icons/puzzle.svg" alt="game English puzzle" />
+      </div>
     </div>
     <div className="games-page__block">
       <h2 className="games-page__heading">Savannah</h2>
-      <img className="game-page__image" src="../../assets/images/logo.svg" alt="game SpeackIt" />
+      <div className="game-page__image-block">
+        <img className="game-page__image" src="../../assets/images/games-page-icons/savannah.svg" alt="game savannah" />
+      </div>
     </div>
     <div className="games-page__block">
       <h2 className="games-page__heading">Audio Call</h2>
-      <img className="game-page__image" src="../../assets/images/logo.svg" alt="game SpeackIt" />
+      <div className="game-page__image-block">
+        <img className="game-page__image" src="../../assets/images/games-page-icons/phone.svg" alt="game Audio Call" />
+      </div>
     </div>
     <div className="games-page__block">
       <h2 className="games-page__heading">Sprint</h2>
-      <img className="game-page__image" src="../../assets/images/logo.svg" alt="game SpeackIt" />
+      <div className="game-page__image-block">
+        <img className="game-page__image" src="../../assets/images/games-page-icons/sprint.svg" alt="game Sprint" />
+      </div>
     </div>
     <div className="games-page__block">
-      <h2 className="games-page__heading">Own Game</h2>
-      <img className="game-page__image" src="../../assets/images/logo.svg" alt="game SpeackIt" />
+      <h2 className="games-page__heading">VR Gaming</h2>
+      <div className="game-page__image-block">
+        <img className="game-page__image" src="../../assets/images/games-page-icons/vr-gaming.svg" alt="game VR Gaming" />
+      </div>
     </div>
   </div>
 );
