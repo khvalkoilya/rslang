c14b9aa93394a0271ff3dcc80fc8fc59d7f57fb0
Andey Amelchenia <pmad1986@gmail.com>
2020-06-29 18:43:43 +0300

    fix: utilsApi

--- src/utils/utilsApi.js
+++ src/utils/utilsApi.js
@@ -1,13 +1,25 @@
 import URL from '../variables/UrlApi';
 
 const data = async (arg) => {
-  const arr = await fetch(...arg)
-    .then((res) => res.json())
-    .then((result) => result)
-    .catch((error) => error);
-  return arr;
+  const res = await fetch(...arg);
+  const result = await res.json();
+  return result;
 };
 
-export const getWordsData = (group = 0, page = 0) => data([URL.getWords(page, group)]);
+export class User {
+  constructor(page, group) {
+    this.page = page || 0;
+    this.group = group || 0;
+  }
+
+  setData(res) {
+    this.data = res;
+  }
+
+  async getWordsData() {
+    data([URL.getWords(this.page, this.group)])
+      .then((res) => this.setData(res));
+  }
+}
 
 export const getUrlData = (name) => `https://raw.githubusercontent.com/AndreyAmelchenia/rslang-data/master/${name}`;
