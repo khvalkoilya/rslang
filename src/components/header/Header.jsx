6f490c2d520da642acda2d3c5bce563cd0e74cfc
Андрей Амельченя <60568794+AndreyAmelchenia@users.noreply.github.com>
2020-06-30 00:11:25 +0300

    init:create markup for registration page #52 (#74)
    * feat: add large screen styles for header
    
    * feat: add medium screen styles for header
    
    * feat: add svg && add style constant && add small screen styles for header
    
    * feat: add icons link in userMenuItems && add icons in user menu
    
    * init: form registaration
    
    * fix: Registration.jsx
    
    * fix: Input.jsx , Registration.jsx
    
    Co-authored-by: Artem <uspenskiyart@gmail.com>

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainMenu from '../mainMenu/MainMenu';
 import UserMenu from '../userMenu/UserMenu';
+import BurgerButton from '../burgerButton/BurgerButton';
+import Logo from '../logo/Logo';
 
-const isAuth = true;
+const isAuth = false;
 
-const Header = () => (
-  <div className="header">
-    <div className="header__logo" />
-    <div className="header__main-menu">
-      <MainMenu isAuth={isAuth} />
+const Header = () => {
+  const [visible, setVisible] = useState(false);
+  const Click = () => setVisible(!visible);
+  return (
+    <div className="header">
+      <BurgerButton click={Click} />
+      <div className="header__logo">
+        <Logo />
+      </div>
+      <nav className={`nav ${visible ? 'visible' : 'unvisible'}`}>
+        <div className="header__main-menu">
+          <MainMenu isAuth={isAuth} />
+        </div>
+        <div className="header__profile-menu">
+          <UserMenu isAuth={isAuth} />
+        </div>
+      </nav>
     </div>
-    <div className="header__profile-menu">
-      <UserMenu isAuth={isAuth} />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Header;
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Registration from '../registration/Registration';
 
-const Main = () => (
+const Main = ({ page }) => (
   <main className="main">
-    this is main
+    {page === 'games' && <div>Games</div>}
+    {page === 'settings' && <input type="text" />}
+    {page === 'statistics' && <div>statistics</div>}
+    {page === 'logOut' && <div>logOut</div>}
+    {page === 'signIn' && <Registration state={page} />}
+    {page === 'registration' && <Registration state={page} />}
+    {page === 'train' && <div>train</div>}
+    {page === 'vocabulary' && <div>settings</div>}
   </main>
 );
 
+Main.propTypes = {
+  page: PropTypes.string.isRequired,
+};
 export default Main;
