f215d60229dee7bd6437650545e52a050d31c961
khvalkoilya <khvalko.ilya@gmail.com>
2020-06-30 19:45:29 +0300

    feat: add const with default constants

---
+++ src/variables/defaultSettings.js
@@ -0,0 +1,21 @@
+import DEFAULT_WORDS from './defaultWords';
+
+export default DEFAULT_SETTINGS = {
+  wordsPedDay: 20,
+  optional: {
+    translation: true,
+    meaning: true,
+    example: true,
+    transcription: true,
+    image: true,
+    delete: true,
+    difficult: true,
+    autoSpeech: true,
+    autoTranslation: true,
+    showing: true,
+    intervalAgain: true,
+    intervalHard: true,
+    intervalGood: true,
+    intervalEasy: true,
+  },
+};
