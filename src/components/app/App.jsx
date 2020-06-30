378fe880883a5a27eb3ae6d1669eaf8f81c0abc4
Ivan <kan.ivann@gmail.com>
2020-06-30 17:40:16 +0300

    feat: add speakIt start page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
+import StartPageSpeakIt from '../speakItStartPage/SpeakItStartPage';
 
 const App = () => (
   <>
     <Header />
+    <StartPageSpeakIt />
     <Footer />
   </>
 );
