651ee211410d2d58caee10403ea465a8683d6a95
Andey Amelchenia <pmad1986@gmail.com>
2020-06-30 14:37:48 +0300

    fix: User.js

--- src/User/User.js
+++ src/User/User.js
@@ -1,14 +1,19 @@
 import URL from './urlApi';
-import { fetchData } from './utillsApi';
+import { fetchData } from './utilsApi';
 
 class User {
+  constructor() {
+    this.page = 0;
+    this.group = 0;
+  }
+
   setData(res) {
     this.data = res;
   }
 
   async getWordsData(page, group) {
-    this.page = page || 0;
-    this.group = group || 0;
+    this.page = page || this.page;
+    this.group = group || this.group;
     fetchData([URL.getWords(this.page, this.group)])
       .then((res) => this.setData(res));
   }
