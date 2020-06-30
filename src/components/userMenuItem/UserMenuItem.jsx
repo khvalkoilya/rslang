9d1d41b21b05d06fbb39b5a2141b1d68ab7e4220
Artem <uspenskiyart@gmail.com>
2020-06-30 15:14:29 +0300

    feat: change styles for main mene item

--- src/components/userMenuItem/UserMenuItem.jsx
+++ src/components/userMenuItem/UserMenuItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { Page } from '../context/Context';
 import USER_MENU_ITEMS from '../../variables/userMenuItems';
 
 const UserMenuItem = ({ isAuth }) => {
