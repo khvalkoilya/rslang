e4b25c9a953becaa8b8ac62e82d4b5c3547be811
Aleksand Sumaneev <cahobad@ya.ru>
2020-07-04 21:16:50 +0300

    feat: add strucrure and base style

--- src/components/sprintGame/SprintGame.jsx
+++ src/components/sprintGame/SprintGame.jsx
@@ -1,14 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import words from '../../variables/defaultWords';
 
 // https://youtu.be/ynmb9pb2mUs
 
-const Sprint = () => (
+function Sprint() {
+  const [seconds, setSeconds] = useState(60);
 
-  <div className="sprint-game">
-    {words[3].word}
-  </div>
+  const [level, setLevel] = useState(0);
 
-);
+  const [score, setScore] = useState(0);
+
+  const [currentWord, setCurrentWord] = useState([]);
+
+  const gameStep = () => {
+    console.log('нажали на кнопку');
+    if (level === 0) {
+      setLevel(1);
+      startTimer();
+      setLevel(level + 1);
+    }
+  };
+
+  const startTimer = () => {
+    const x = seconds - 1;
+    if (x === 0) {
+      endGame();
+    } else {
+      setInterval(() => {
+        console.log(x);
+        setSeconds(x);
+      }, 1000);
+    }
+  };
+
+  return (
+    <div className="sprint-game">
+      <div className="sprint-game__header">
+        <div className="sprint-game__points">
+          {score}
+        </div>
+
+        <div className="sprint-game__level">
+          {level}
+        </div>
+
+        <div className="sprint-game__timer">
+          {seconds}
+        </div>
+      </div>
+      <div className="sprint-game__words">
+        <p className="sprint-game__english-word">
+          Cat
+        </p>
+        <p className="sprint-game__russian-word">
+          Кошка
+        </p>
+      </div>
+      <div className="sprint-game__control">
+        <button onClick={gameStep} className="sprint-game__button sprint-game__button--no" type="button">Нет</button>
+        <button onClick={gameStep} className="sprint-game__button sprint-game__button--yes" type="button">Да</button>
+      </div>
+
+    </div>
+  );
+}
 
 export default Sprint;
