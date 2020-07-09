fb87d81e280008fefed57536020ca3b833886006
Aleksand Sumaneev <cahobad@ya.ru>
2020-07-09 22:20:03 +0300

    feat: add video and text about us

--- src/components/demoPage/DemoPage.jsx
+++ src/components/demoPage/DemoPage.jsx
@@ -2,7 +2,36 @@ import React from 'react';
 
 const DemoPage = () => (
   <div className="demo-page">
-    demo page
+    <div className="demo-page__youtube">
+      <iframe title="demo" width="560" height="315" src="https://www.youtube.com/embed/57T_njnD_T0" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+    </div>
+    <div className="demo-page__about-app">
+      <h1 className="demo-page__heading">RSLang</h1>
+      <p>
+        RSLang - онлайн-сервис для изучения и практики английского языка
+      </p>
+      <p>
+        Все тренировки интерактивны и основаны на проверенных методиках!
+      </p>
+      <p>
+        Вы сможете создать личный словарь, пройти увлекательное обучение. В специальном разделе наглядно виден прогресс обучения
+      </p>
+      <p>
+        RSLang - это Эффективно и Увлекательно!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur itaque repudiandae, quod soluta dignissimos magnam, nostrum minima earum architecto dolorem ullam molestias vel tempora maiores voluptatem hic neque consequuntur optio?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur itaque repudiandae, quod soluta dignissimos magnam, nostrum minima earum architecto dolorem ullam molestias vel tempora maiores voluptatem hic neque consequuntur optio?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur itaque repudiandae, quod soluta dignissimos magnam, nostrum minima earum architecto dolorem ullam molestias vel tempora maiores voluptatem hic neque consequuntur optio?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur itaque repudiandae, quod soluta dignissimos magnam, nostrum minima earum architecto dolorem ullam molestias vel tempora maiores voluptatem hic neque consequuntur optio?
+      </p>
+    </div>
   </div>
 );
 
