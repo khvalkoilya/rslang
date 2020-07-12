b7ddcf4b39e555fb6a3437efd5451c4da83a860c
Андрей Амельченя <60568794+AndreyAmelchenia@users.noreply.github.com>
2020-07-13 00:22:45 +0300

    feat: create backend for sign in (#58)
    * feat: add utilsApi.js, const UrlApi.js
    
    * fix: utilsApi.js add function getUrlData
    
    * fix: utilsApi.js, urlApi.js
    
    * fix: function getWordsData
    
    * fix: function getWordsData
    
    * fix: UrlApi, utilsApi
    
    * fix: utilsApi, UrlApi
    
    * fix: utilsApi add function createUser
    
    * fix: utilsApi add function loginUser
    
    * fix: utilsApi
    
    * fix: utilsApi
    
    * fix: utilsApi
    
    * fix: utilsApi
    
    * fix: utilsApi
    
    * feat: User.js, folder User
    
    * fix: User.js
    
    * fix: User
    
    * fix: function createUser
    
    * feat: utilsApi

---
+++ src/variables/UrlApi.js
@@ -0,0 +1,62 @@
+const BASE_PATH = 'https://afternoon-falls-25894.herokuapp.com'; // 'http://pacific-castle-12388.herokuapp.com';// 'https://afternoon-falls-25894.herokuapp.com';
+
+const URL = {
+  getWords: (page, group) => `${BASE_PATH}/words?page=${page}&group=${group}`,
+  userApi: {
+    createUser: `${BASE_PATH}/users`,
+    loginUser: `${BASE_PATH}/signin`,
+    headerUser: (user) => ({
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    }),
+  },
+  userSetting: {
+    settings: (idUser) => `${BASE_PATH}/users/${idUser}/settings`,
+    putSettings: (token, option) => ({
+      method: 'PUT',
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(option),
+    }),
+    getSettings: (token) => ({
+      method: 'GET',
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }),
+  },
+  userStatistic: {
+    statistics: (idUser) => `${BASE_PATH}/users/${idUser}/statistics`,
+    putStatistics: (option, token) => ({
+      method: 'PUT',
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(option),
+    }),
+    getStatistics: (token) => ({
+      method: 'GET',
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+      },
+    }),
+  },
+};
+
+export default URL;
